163ac7ac7e752bdd11bd5ea798efa4b126cc97e5
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-02-02 21:36:43 +0100

    fixed errors in SLURM trainiong script.

--- train.sh
+++ train.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=11G
 #SBATCH --gres=gpu:1
-# SBATCH --partition=gpu-cluster
-# SBATCH --output=out/train.out
-# SBATCH --time=12:00:00
+#SBATCH --partition=gpu-cluster
+#SBATCH --output=out/train.out
+#SBATCH --time=12:00:00
 
 HOME=/clusterstorage/gkobsik
 source $HOME/.bashrc
 
 conda activate research
-python src/main.py train configs/mnist_xxs.yml
+python main.py train configs/mnist_xs.yml
 # tail -f out/train.out
