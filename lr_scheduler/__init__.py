7b3d626094e73e4c8ec74f8a0672f0667e45f173
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-02-10 13:00:29 +0100

    update lr_schedular module with constant lr_scheduler

--- lr_scheduler/__init__.py
+++ lr_scheduler/__init__.py
@@ -1,5 +1,7 @@
 from lr_scheduler.cosine_annealing_with_warmup import CosineAnnealingWarmupRestarts
+from lr_scheduler.constant_with_warmup import ConstantWithWarmup
 
 __all__ = [
     "CosineAnnealingWarmupRestarts",
+    "ConstantWithWarmup",
 ]
