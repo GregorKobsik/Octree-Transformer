472f0c8666c8da76d4c50f497ed9bd1d82dde717
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-03-07 20:35:27 +0100

    append: rename to basic transformer

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
-from models.shape_transformer_model import ShapeTransformerModel
 from models.shape_transformer import ShapeTransformer
+from models.basic_shape_transformer_model import BasicShapeTransformerModel
 from models.fast_shape_transformer_model import FastShapeTransformerModel
 
 __all__ = [
-    "ShapeTransformerModel",
     "ShapeTransformer",
+    "BasicShapeTransformerModel",
     "FastShapeTransformerModel",
 ]
