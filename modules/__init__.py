46b02db6ddf35e6fe387fb4b92e6f8fea416f5b6
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-05-05 17:30:49 +0200

    append init files

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,21 +1,5 @@
-from modules.basic_encoder_only_module import BasicEncoderOnlyModule
-from modules.basic_encoder_decoder_module import BasicEncoderDecoderModule
-from modules.fast_encoder_only_module import FastEncoderOnlyModule
-from modules.performer_encoder_only_module import PerformerEncoderOnlyModule
-from modules.reformer_encoder_only_module import ReformerEncoderOnlyModule
-from modules.routing_encoder_only_module import RoutingEncoderOnlyModule
-from modules.sinkhorn_encoder_only_module import SinkhornEncoderOnlyModule
-from modules.linear_encoder_only_module import LinearEncoderOnlyModule
 from modules.shape_transformer import ShapeTransformer
 
 __all__ = [
-    "BasicEncoderOnlyModule",
-    "BasicEncoderDecoderModule",
-    "FastEncoderOnlyModule",
-    "PerformerEncoderOnlyModule",
-    "ReformerEncoderOnlyModule",
-    "RoutingEncoderOnlyModule",
-    "SinkhornEncoderOnlyModule",
-    "LinearEncoderOnlyModule",
     "ShapeTransformer",
 ]
---
+++ modules/encoder_decoder/__init__.py
@@ -0,0 +1,5 @@
+from modules.encoder_decoder.basic_encoder_decoder_module import BasicEncoderDecoderModule
+
+__all__ = [
+    "BasicEncoderDecoderModule",
+]
