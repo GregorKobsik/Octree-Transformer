03fe98ea72dbd8245dd9cf62561af6e3412d40c6
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-05-08 12:51:04 +0200

    bug: Fixed quadtree tests after major changes in the API
    pay attention to padding token '0' now
    no return 'as_string' anymore

--- tests/quadtree_test.py
+++ tests/quadtree_test.py
@@ -2,11 +2,11 @@ import unittest
 import numpy as np
 import numpy.testing as np_test
 import matplotlib.image as mpimg
-from utils.quadtree import QuadTree
+from utils.quadtree import Quadtree
 
 
 def binarize(x):
-    return np.add(x > 0.1, x == 1, dtype=int)
+    return np.add(x > 0.1, x == 1, dtype=int) + 1
 
 
 def mnist_28x28():
@@ -33,53 +33,51 @@ class TestDataAvailable(unittest.TestCase):
 
 class TestQuadtreeCreation(unittest.TestCase):
     def test_mnist_28x28(self):
-        qtree = QuadTree().insert_image(mnist_28x28())
-        np_test.assert_array_equal(qtree.north_west.north_west.value, 0)
-        np_test.assert_array_equal(qtree.south_west.south_west.value, 0)
+        qtree = Quadtree().insert_image(mnist_28x28())
+        np_test.assert_array_equal(qtree.north_west.north_west.value, 1)
+        np_test.assert_array_equal(qtree.south_west.south_west.value, 1)
 
     def test_mnist_32x32(self):
-        qtree = QuadTree().insert_image(mnist_32x32())
-        np_test.assert_array_equal(qtree.north_west.north_west.value, 0)
-        np_test.assert_array_equal(qtree.south_west.south_west.value, 0)
-        np_test.assert_array_almost_equal(qtree.south_west.north_east.north_east.north_east.value, 2)
+        qtree = Quadtree().insert_image(mnist_32x32())
+        np_test.assert_array_equal(qtree.north_west.north_west.value, 1)
+        np_test.assert_array_equal(qtree.south_west.south_west.value, 1)
+        np_test.assert_array_almost_equal(qtree.south_west.north_east.north_east.north_east.value, 3)
 
 
 class TestImageRetrival(unittest.TestCase):
     def test_image_shape_28x28(self):
         img = mnist_28x28()
-        qtree = QuadTree().insert_image(img)
+        qtree = Quadtree().insert_image(img)
         self.assertEqual(qtree.get_image(10).shape, img.shape)
 
     def test_image_shape_32x32(self):
         img = mnist_32x32()
-        qtree = QuadTree().insert_image(img)
+        qtree = Quadtree().insert_image(img)
         self.assertEqual(qtree.get_image(10).shape, img.shape)
 
     def test_image_shape_32x32_binarized(self):
         img_bin = mnist_32x32_binarized()
-        qtree = QuadTree().insert_image(img_bin)
+        qtree = Quadtree().insert_image(img_bin)
         self.assertEqual(qtree.get_image(10).shape, img_bin.shape)
 
     def test_image2sequence2image_shape(self):
         img_bin = mnist_32x32_binarized()
-        qtree = QuadTree().insert_image(img_bin)
-        qtree = QuadTree().insert_sequence(qtree.get_sequence(), resolution=img_bin.shape)
+        qtree = Quadtree().insert_image(img_bin)
+        qtree = Quadtree().insert_sequence(qtree.get_sequence(), resolution=img_bin.shape)
         np_test.assert_array_equal(qtree.get_image(10).shape, img_bin.shape)
 
 
 class TestSequenceRetrival(unittest.TestCase):
-    def test_sequence_depth2(self):
-        seq = "111110001001001011010"
-        qtree = QuadTree().insert_image(mnist_32x32_binarized())
-        self.assertSequenceEqual(qtree.get_sequence(depth=2, as_string=True), seq)
-        self.assertSequenceEqual(''.join(str(x) for x in qtree.get_sequence(depth=2)), seq)
+    def test_sequence_depth3(self):
+        seq = "222221112112112122121"
+        qtree = Quadtree().insert_image(mnist_32x32_binarized())
+        self.assertSequenceEqual(''.join(str(x) for x in qtree.get_sequence(depth=3)), seq)
 
     def test_diagonal_sequence(self):
-        seq = "1" + "0110" + "01100110" + "0110011001100110" + "01100110011001100110011001100110" \
-            + "0110011001100110011001100110011001100110011001100110011001100110"
+        seq = "2" + "1221" + "12211221" + "1221122112211221" + "12211221122112211221122112211221" \
+            + "1221122112211221122112211221122112211221122112211221122112211221"
         shape = (32, 32)
-        qtree = QuadTree().insert_sequence(seq, shape)
-        self.assertSequenceEqual(qtree.get_sequence(as_string=True), seq)
+        qtree = Quadtree().insert_sequence(seq, shape)
         self.assertSequenceEqual(''.join(str(x) for x in qtree.get_sequence()), seq)
 
 
@@ -95,9 +93,9 @@ class TestErroneousSequences(unittest.TestCase):
 
         Error resolved, when checking the size of the open_set to decide if the parser is done.
         """
-        seq = [1, 0, 0, 0, 0, 0]
+        seq = [2, 1, 1, 1, 1, 1]
         shape = (32, 32)
-        qtree = QuadTree().insert_sequence(seq, shape, autorepair_errors=True, silent=True)
+        qtree = Quadtree().insert_sequence(seq, shape, autorepair_errors=True, silent=True)
         qtree.get_image(1)
         self.assertTrue(True)
 
@@ -107,13 +105,13 @@ class TestErroneousSequences(unittest.TestCase):
         Error resolved, when final nodes are assigned at resolution (1,1), too.
         """
         seq = [
-            1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0,
-            1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1,
-            1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
-            1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-            1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0
+            2, 1, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 1, 1, 2, 1, 2, 2, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 2, 2, 1, 2, 1, 1, 2, 1,
+            2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1, 2, 2,
+            2, 2, 2, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1,
+            2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+            2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1
         ]
         shape = (32, 32)
-        qtree = QuadTree().insert_sequence(seq, shape, autorepair_errors=True, silent=True)
+        qtree = Quadtree().insert_sequence(seq, shape, autorepair_errors=True, silent=True)
         qtree.get_image(1)
         self.assertTrue(True)
