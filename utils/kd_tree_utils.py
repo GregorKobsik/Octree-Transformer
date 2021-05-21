ba1f18a0831ecfaffa3c3dec140e4063cacb00c3
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-05-21 12:18:23 +0200

    Extend representation transformer with a wrapper for pytroch tensors.
    update function descriptions

--- utils/kd_tree_utils.py
+++ utils/kd_tree_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+import torch
 
 
 def _directions(spatial_dim):
@@ -8,6 +9,11 @@ def _directions(spatial_dim):
 
 class RepresentationTransformator():
     def __init__(self, spatial_dim=3):
+        """ Provides a transformation wrapper between the iterative and successive sequence format.
+
+        Args:
+            spatial_dim: Define the spatial dimensionality of the input sequences.
+        """
         super().__init__()
         self.spatial_dim = spatial_dim
         self.num_tokens = 2**spatial_dim
@@ -33,9 +39,14 @@ class RepresentationTransformator():
     def successive_to_iterative(self, value, depth, position):
         """ Transforms given successive sequence into an iterative sequence representation.
 
+        Args:
+            value: Numpy array holding the value token sequence with shape (S), with token values in [0, 3]
+            depth: Numpy array holding the depth token sequence with shape (S).
+            position: Numpy array holding the position token sequence with shape (S, spatial_dim).
+
         Return:
-            A tuple of (value, depth, position, target),
-            where the last depth layer is encoded in target in a trinary format representation.
+            A tuple of (value, depth, position, target), where the last depth layer is encoded in target in a trinary
+            format representation.
         """
         max_depth = np.max(depth)
         target = []
@@ -63,12 +74,47 @@ class RepresentationTransformator():
 
         return value, depth, position, np.array(target)
 
-    def iterative_to_successive(self, value, depth, pos, target):
-        """ Transforms given iterative sequence into an successive sequence representation.
+    def successive_to_iterative_pytorch(self, value, depth, position):
+        """ Transforms given successive sequence into an iterative sequence representation. Provides a wrapper for
+            pytorch tensors.
+
+        Args:
+            value: Pytorch tensor holding the value token sequence with shape (S), with token values in [0, 3].
+            depth: Pytorch tensor holding the depth token sequence with shape (S).
+            position: Pytorch tensor holding the position token sequence with shape (S, spatial_dim).
 
         Return:
-            A tuple of (value, depth, position),
-            where the target sequence is encoded in the last depth layer of the new sequence.
+            A tuple of (value, depth, position, target), where the last depth layer is encoded in target in a trinary
+            format representation.
+        """
+        device = value.device
+
+        value = value.cpu().numpy()
+        depth = depth.cpu().numpy()
+        position = position.cpu().numpy()
+
+        value, depth, position, target = self.successive_to_iterative(value, depth, position)
+
+        value = torch.tensor(value, dtype=torch.long, device=device)
+        depth = torch.tensor(depth, dtype=torch.long, device=device)
+        position = torch.tensor(position, dtype=torch.long, device=device)
+        target = torch.tensor(target, dtype=torch.long, device=device)
+
+        return value, depth, position, target
+
+    def iterative_to_successive(self, value, depth, position, target):
+        """ Transforms given iterative sequence into an successive sequence representation.
+
+        Args:
+            value: Numpy array holding the value token sequence with shape (S), with token values in [0, 3].
+            depth: Numpy array holding the depth token sequence with shape (S).
+            position: Numpy array holding the position token sequence with shape (S, spatial_dim).
+            target: Numpy array holding the value token sequence of the next layer with the shape (S), with token
+                values in [0, 3**2**spatial_dim].
+
+        Return:
+            A tuple of (value, depth, position), where the target sequence is encoded in the last depth layer of the
+            new sequence.
         """
         max_depth = np.max(depth)
 
@@ -77,11 +123,11 @@ class RepresentationTransformator():
         next_layer_pos = np.array([])
 
         # compute how much does the position per token change in the next layer
-        pos_step = pos[0][0] // 2**(max_depth)  # assume same resolution for each dimension
+        pos_step = position[0][0] // 2**(max_depth)  # assume same resolution for each dimension
 
         # retrive values and positions of last layer from the sequence
         last_layer_value = value[depth == max_depth]
-        last_layer_pos = pos[depth == max_depth]
+        last_layer_pos = position[depth == max_depth]
 
         # parse the last layer and target sequence to decode next layer
         for i, token in enumerate(last_layer_value):
@@ -94,6 +140,36 @@ class RepresentationTransformator():
         # concatenate sequences and return
         value = np.concatenate([value, next_layer_value])
         depth = np.concatenate([depth, next_layer_depth])
-        pos = np.concatenate([pos, next_layer_pos])
+        position = np.concatenate([position, next_layer_pos])
 
-        return value, depth, pos
+        return value, depth, position
+
+    def iterative_to_successive_pytorch(self, value, depth, position, target):
+        """ Transforms given iterative sequence into an successive sequence representation. Provides a wrapper for
+            pytorch tensors.
+
+        Args:
+            value: Pytorch tensor holding the value token sequence with shape (S), with token values in [0, 3].
+            depth: Pytorch tensor holding the depth token sequence with shape (S).
+            position: Pytorch tensor holding the position token sequence with shape (S, spatial_dim).
+            target: Pytorch tensor holding the value token sequence of the next layer with the shape (S), with token
+                values in [0, 3**2**spatial_dim].
+
+        Return:
+            A tuple of (value, depth, position), where the target sequence is encoded in the last depth layer of the
+            new sequence.
+        """
+        device = value.device
+
+        value = value.cpu().numpy()
+        depth = depth.cpu().numpy()
+        position = position.cpu().numpy()
+        target = target.cpu().numpy()
+
+        value, depth, position = self.iterative_to_successive(value, depth, position, target)
+
+        value = torch.tensor(value, dtype=torch.long, device=device)
+        depth = torch.tensor(depth, dtype=torch.long, device=device)
+        position = torch.tensor(position, dtype=torch.long, device=device)
+
+        return value, depth, position
