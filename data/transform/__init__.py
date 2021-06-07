7d00be1130005e8ac779c3335784c1492cf558f0
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-06-07 15:31:35 +0200

    rename convolutional data transform

--- data/transform/__init__.py
+++ data/transform/__init__.py
@@ -1,5 +1,5 @@
 from data.transform.basic_transform import BasicTransform
-from data.transform.convolutional_transform import DoubleConvolutionalTransform
+from data.transform.double_conv_transform import DoubleConvolutionalTransform
 
 __all__ = [
     "BasicTransform",
