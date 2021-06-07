6c2a99c1744cbdc8a3db444e3e3225ae3a1b8e36
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-06-07 13:20:31 +0200

    adjust API and description of padding mask.
    expects now the batch dimension to be always first.

--- masks/padding_mask.py
+++ masks/padding_mask.py
@@ -4,12 +4,6 @@ import torch
 def padding_mask(input_sequence, device=None):
     """ Create a padding mask for the given input.
 
-        Always assumens '0' as a padding value.
-        input_sequence has the shape (S, N)
-
-        PyTorch Transformer defines 'src_key_padding_mask' with shape (N, S),
-        where the input shape of 'src' is (S, N, E).
-        Therefor we need to transpose the dimensions of the created mask.
-        """
-    mask = torch.zeros_like(input_sequence, device=device).masked_fill(input_sequence == 0, 1).bool()  # [S, N]
-    return mask.transpose(0, 1)  # [N, S]
+        Always assumens '0' as a padding value. `input_sequence` has the shape (N, S).
+    """
+    return torch.zeros_like(input_sequence, device=device).masked_fill(input_sequence == 0, 1).bool()
