1b54bf4323f550a737b1f0975b95cf512c82b79a
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-06-08 17:22:29 +0200

    implement sampler for single convolutional embedding

--- sample/sampler/__init__.py
+++ sample/sampler/__init__.py
@@ -1,7 +1,9 @@
 from sample.sampler.basic_encoder_decoder_sampler import BasicEncoderDecoderSampler
+from sample.sampler.single_conv_encoder_decoder_sampler import SingleConvEncoderDecoderSampler
 from sample.sampler.double_convolutional_encoder_decoder_sampler import DoubleConvolutionalEncoderDecoderSampler
 
 __all__ = [
     "BasicEncoderDecoderSampler",
+    "SingleConvEncoderDecoderSampler",
     "DoubleConvolutionalEncoderDecoderSampler",
 ]
--- sample/sampler/single_conv_encoder_decoder_sampler.py
+++ sample/sampler/single_conv_encoder_decoder_sampler.py
@@ -2,16 +2,17 @@ import math
 import torch
 
 from tqdm.auto import tqdm
-from utils import kdTree
 from sample.sample_utils import next_layer_tokens
 
+from sample.sampler import BasicEncoderDecoderSampler
 
-class BasicEncoderDecoderSampler:
+
+class SingleConvEncoderDecoderSampler(BasicEncoderDecoderSampler):
     def __init__(self, spatial_dim, device, max_tokens, max_resolution, model):
         """ Provides a basic implementation of the sampler for the encoder decoder architecture with all basic modules.
 
         The following sampler works with the following combinations of modules [architecture, embedding, head]:
-            - 'encoder_decoder', 'basic', 'generative_basic'
+            - 'encoder_decoder', 'single_conv', 'single_conv'
 
         Args:
             spatial_dim: Spatial dimensionality of the array of elements.
@@ -20,57 +21,8 @@ class BasicEncoderDecoderSampler:
             max_resolution: Maximum resolution the model is trained on
             model: Instance of model which is used for sampling.
         """
-        self.spatial_dim = spatial_dim
-        self.device = device
-        self.max_tokens = max_tokens
-        self.max_resolution = max_resolution
-        self.model = model
-
-    def __call__(self, precondition, precondition_resolution, target_resolution, temperature):
-        """ Sample a single example based on the given `precondition` up to `target_resolution`.
-
-        Args:
-            precondition: An array of elements (pixels/voxels) as an numpy array.
-            precondition_resolution: Resolution, to which the input array will be downscaled and used as a precondition
-                for sampling.
-            target_resolution: Resolution up to which an object should be sampled.
-            temperature: Defines the randomness of the samples.
-
-        Return:
-            An array of elements as a numpy array, which represents the sampled shape.
-        """
-        # preprocess the input and transform it into a token sequence
-        sequence = self.preprocess(precondition, precondition_resolution)
-
-        # enhance the resolution of the sequence or generate a new sequence by sampling new token values
-        value = self.sample(sequence, target_resolution, temperature)
-
-        # postprocess the token value sequence and return it as an array of elements
-        return self.postprocess(value, target_resolution)
-
-    def preprocess(self, precondition, precondition_resolution):
-        """ Transform input array elements into token sequences.
-
-        Args:
-            precondition: An array of elements (pixels/voxels) as an numpy array.
-            precondition_resolution: Resolution, to which the input array will be downscaled and used as a precondition
-                for sampling.
-
-        Return:
-            PyTorch tensor consisting of token sequences: (value, depth, position).
-        """
-        # convert input array into token sequence
-        tree = kdTree(self.spatial_dim).insert_element_array(precondition)
-        value, depth, pos = tree.get_token_sequence(
-            depth=math.log2(precondition_resolution), return_depth=True, return_pos=True
-        )
-
-        # convert sequence tokens to PyTorch as a long tensor
-        value = torch.tensor(value, dtype=torch.long, device=self.device)
-        depth = torch.tensor(depth, dtype=torch.long, device=self.device)
-        pos = torch.tensor(pos, dtype=torch.long, device=self.device)
-
-        return value, depth, pos
+        super(SingleConvEncoderDecoderSampler, self).__init__(spatial_dim, device, max_tokens, max_resolution, model)
+        self.num_sampled_tokens = 2**spatial_dim
 
     def sample(self, sequences, target_resolution, temperature):
         """ Perform an iterative sampling of the given sequence until reaching the end of sequence, the maximum sequence
@@ -107,7 +59,9 @@ class BasicEncoderDecoderSampler:
 
                 # sample autoregressive tokens for the next layer
                 # TODO: check indizes - probably correct: range(len(tgt_val) - 1)
-                for cur_token_idx in tqdm(range(len(tgt_val)), leave=False, desc="Sampling"):
+                for cur_token_idx in tqdm(
+                    range(0, len(tgt_val), self.num_sampled_tokens), leave=False, desc="Sampling"
+                ):
 
                     # compute decoder output
                     latent_sequence = self.process_decoder(tgt_val, tgt_depth, tgt_pos, memory, cur_token_idx)
@@ -126,26 +80,6 @@ class BasicEncoderDecoderSampler:
 
         return value
 
-    def process_encoder(self, value, depth, pos):
-        """ Process the encoder part of the model and return the encoded sequence as memory in encoder latent space.
-
-        Args:
-            value: Value token sequence.
-            depth: Depth token sequence.
-            pos: Position token sequence.
-
-        Return:
-            Latent sequence vector representing already sampled layers of token sequences.
-        """
-        # precompute encoder memory / process input values sequence
-        memory = self.model.encode(
-            value.unsqueeze(0),  # [N, S]
-            depth.unsqueeze(0),  # [N, S]
-            pos.unsqueeze(0),  # [N, S, A]
-        )[0]  # [N, S', E]
-
-        return memory
-
     def process_decoder(self, tgt_val, tgt_depth, tgt_pos, memory, token_idx):
         """ Process the decoder part of the model and return the decoded target sequence in decoder latent space.
 
@@ -161,9 +95,9 @@ class BasicEncoderDecoderSampler:
         """
         # decode target sequence and memory into latent space
         latent_sequence = self.model.decode(
-            tgt_val[:token_idx + 1].unsqueeze(0),  # [N, T]
-            tgt_depth[:token_idx + 1].unsqueeze(0),  # [N, T]
-            tgt_pos[:token_idx + 1].unsqueeze(0),  # [N, T, A]
+            tgt_val[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T]
+            tgt_depth[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T]
+            tgt_pos[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T, A]
             memory.unsqueeze(0),  # [N, S', E]
         )[0]  # [T', E]
 
@@ -180,14 +114,14 @@ class BasicEncoderDecoderSampler:
             token_idx: Currently sampled token index.
 
         Return:
-            Logits of already sampled tokens and currently sampled token.
+            Logits of already sampled tokens and currently sampled tokens.
         """
         # compute logits from latent sequence
         logits = self.model.head(
-            latent_sequence[:token_idx + 1].unsqueeze(0),  # [N, T', E]
-            tgt_val[:token_idx + 1].unsqueeze(0),  # [N, T]
-            tgt_depth[:token_idx + 1].unsqueeze(0),  # [N, T]
-            tgt_pos[:token_idx + 1].unsqueeze(0),  # [N, T, A]
+            latent_sequence[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T', E]
+            tgt_val[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T]
+            tgt_depth[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T]
+            tgt_pos[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T, A]
         )[0]  # [T, V]
 
         return logits
@@ -202,35 +136,18 @@ class BasicEncoderDecoderSampler:
             temperature: Defines the randomness of the samples.
 
         Return:
-            Updated target token sequence, where the sampled token is exchanged with a sample.
+            Updated target token sequence, where the sampled tokens are exchanged with a sample.
         """
-        last_token_logits = logits[-1]
+        last_token_logits = logits[-self.num_sampled_tokens:]
 
         # compute token probabilities from logits
         probs = torch.nn.functional.softmax(last_token_logits / temperature, dim=0)  # [V]
 
         # zero probability for special tokens -> invalid with parent token
-        probs[0] = 0  # 'padding' token
+        probs[:, 0] = 0  # 'padding' token
 
         # sample next sequence token
-        tgt_val[token_idx] = torch.multinomial(probs, num_samples=1)[0]
+        for i in range(self.num_sampled_tokens):
+            tgt_val[token_idx + i] = torch.multinomial(probs[i], num_samples=1)[0]
+
         return tgt_val
-
-    def postprocess(self, value, target_resolution):
-        """ Transform sequence of value tokens into an array of elements (voxels/pixels).
-
-        Args:
-            value: Value token sequence as a pytorch tensor.
-            target_resolution: Resolution up to which an object should be sampled.
-
-        Return:
-            An array of elements as a numpy array.
-        """
-        tree = kdTree(self.spatial_dim)
-        tree = tree.insert_token_sequence(
-            value.cpu().numpy(),
-            resolution=target_resolution,
-            autorepair_errors=True,
-            silent=True,
-        )
-        return tree.get_element_array(mode="occupancy")
