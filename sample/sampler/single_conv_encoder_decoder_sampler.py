fix bug: softmax over wrong dimension
refactoring: unnecessary slicing of latent_sequence

sample/sampler/single_conv_encoder_decoder_sampler.py
@@ -44,7 +44,7 @@ class SingleConvEncoderDecoderSampler(BasicEncoderDecoderSampler):
 
         with torch.no_grad():
             # sample new tokens layer by layer - encoder: finished layers, decoder: unfinished/sampled layer
-            for _ in tqdm(range(cur_layer, max_layer), initial=cur_layer, total=max_layer, leave=True, desc="Layer"):
+            for _ in tqdm(range(cur_layer, max_layer), initial=cur_layer, total=max_layer, leave=True, desc="Layers"):
 
                 # init sequences for next layer
                 tgt_val, tgt_depth, tgt_pos = next_layer_tokens(
@@ -58,10 +58,7 @@ class SingleConvEncoderDecoderSampler(BasicEncoderDecoderSampler):
                     return value
 
                 # sample autoregressive tokens for the next layer
-                # TODO: check indizes - probably correct: range(len(tgt_val) - 1)
-                for cur_token_idx in tqdm(
-                    range(0, len(tgt_val), self.num_sampled_tokens), leave=False, desc="Sampling"
-                ):
+                for cur_token_idx in tqdm(range(0, len(tgt_val), self.num_sampled_tokens), leave=False, desc="Tokens"):
 
                     # compute decoder output
                     latent_sequence = self.process_decoder(tgt_val, tgt_depth, tgt_pos, memory, cur_token_idx)
@@ -118,7 +115,7 @@ class SingleConvEncoderDecoderSampler(BasicEncoderDecoderSampler):
         """
         # compute logits from latent sequence
         logits = self.model.head(
-            latent_sequence[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T', E]
+            latent_sequence.unsqueeze(0),  # [N, T', E]
             tgt_val[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T]
             tgt_depth[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T]
             tgt_pos[:token_idx + self.num_sampled_tokens].unsqueeze(0),  # [N, T, A]
@@ -141,7 +138,7 @@ class SingleConvEncoderDecoderSampler(BasicEncoderDecoderSampler):
         last_token_logits = logits[-self.num_sampled_tokens:]
 
         # compute token probabilities from logits
-        probs = torch.nn.functional.softmax(last_token_logits / temperature, dim=0)  # [V]
+        probs = torch.nn.functional.softmax(last_token_logits / temperature, dim=-1)  # [t, V]
 
         # zero probability for special tokens -> invalid with parent token
         probs[:, 0] = 0  # 'padding' token