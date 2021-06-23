8f3e488aa5bfab34dfcb83161729b1daf7b06cde
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-06-23 12:11:52 +0200

    implement new embeddings

--- modules/embedding/__init__.py
+++ modules/embedding/__init__.py
@@ -1,4 +1,5 @@
 from modules.embedding.basic_embedding import BasicEmbedding
+from modules.embedding.basic_embedding_B import BasicEmbeddingB
 from modules.embedding.discrete_transformation_embedding import DiscreteTransformationEmbedding
 from modules.embedding.single_conv_embedding_A import SingleConvolutionalEmbeddingA
 from modules.embedding.single_conv_embedding_B import SingleConvolutionalEmbeddingB
@@ -8,12 +9,15 @@ from modules.embedding.single_conv_embedding_E import SingleConvolutionalEmbeddi
 from modules.embedding.single_conv_embedding_F import SingleConvolutionalEmbeddingF
 from modules.embedding.single_conv_embedding_G import SingleConvolutionalEmbeddingG
 from modules.embedding.single_conv_embedding_H import SingleConvolutionalEmbeddingH
+from modules.embedding.single_conv_embedding_I import SingleConvolutionalEmbeddingI
 from modules.embedding.concat_embedding_A import ConcatEmbeddingA
 from modules.embedding.concat_embedding_B import ConcatEmbeddingB
+from modules.embedding.concat_embedding_C import ConcatEmbeddingC
 from modules.embedding.double_conv_embedding import DoubleConvolutionalEmbedding
 
 __all__ = [
     "BasicEmbedding",
+    "BasicEmbeddingB",
     "DiscreteTransformationEmbedding",
     "SingleConvolutionalEmbeddingA",
     "SingleConvolutionalEmbeddingB",
@@ -23,7 +27,9 @@ __all__ = [
     "SingleConvolutionalEmbeddingF",
     "SingleConvolutionalEmbeddingG",
     "SingleConvolutionalEmbeddingH",
+    "SingleConvolutionalEmbeddingI",
     "ConcatEmbeddingA",
     "ConcatEmbeddingB",
+    "ConcatEmbeddingC",
     "DoubleConvolutionalEmbedding",
 ]
