2e4147dac4baefc0f8e7aceb6c699ebd5220f6cf
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-06-28 16:11:42 +0200

    workaround for data transform factory
    probably a circular import

--- data/data.py
+++ data/data.py
@@ -8,7 +8,11 @@ from data import (
     OctreeShapeNet,
 )
 from data.padding import BasicPadding
-from factories import create_data_transform
+from data.transform import (
+    BasicTransform,
+    TrinaryTransform,
+    DoubleConvolutionalTransform,
+)
 
 # Defines a dictionary of available datasets, which can be selected.
 DATASETS = {
@@ -20,6 +24,27 @@ spatial_dim = {
     "mnist": 2,
     "shapenet": 3,
 }
+
+
+def create_data_transform(name, architecture, spatial_dim):
+    """ Creates a data transformation function.
+
+    Args:
+        name: Defines which data transformation function will be created.
+        architecture: Defines which architecture is used in the transformer model.
+        spatial_dim: Spatial dimensionality of input data.
+
+    Return:
+        data transformation function initialised with specified parameters.
+    """
+    if name.startswith(('basic', 'single_conv', 'concat')):
+        return BasicTransform(architecture)
+    elif name.startswith('discrete'):
+        return TrinaryTransform(architecture, spatial_dim)
+    elif name.startswith('double_conv'):
+        return DoubleConvolutionalTransform(architecture)
+    else:
+        raise ValueError(f"ERROR: No data transform for {name} embedding available.")
 
 
 def datasets(
