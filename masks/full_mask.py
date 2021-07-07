6ef279bec55e3ab05a35ff2b06c1ef0ffcbc4ca5
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-07-07 11:59:51 +0200

    fix bug: float instead of int

--- masks/full_mask.py
+++ masks/full_mask.py
@@ -3,4 +3,4 @@ import torch
 
 def full_mask(seq_len, device=None):
     """ Creates a full mask, which allows to access to all tokens. """
-    return torch.full((seq_len, seq_len), 0, device=device)
+    return torch.full((seq_len, seq_len), 0.0, device=device)
