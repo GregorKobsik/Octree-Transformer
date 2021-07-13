fd8ccc2d018ee2322729220658fdb73c99bd632d
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-07-13 17:38:18 +0200

    add preprocess and postprocess to sample_utils

--- sample/sample_utils.py
+++ sample/sample_utils.py
@@ -1,5 +1,10 @@
 import torch
-from utils import _directions
+import math
+
+from utils import (
+    _directions,
+    kdTree,
+)
 
 
 def next_layer_tokens(value, depth, pos, spatial_dim, max_resolution):
@@ -44,3 +49,60 @@ def next_layer_tokens(value, depth, pos, spatial_dim, max_resolution):
     nl_pos = nl_pos + pos_step * torch.tensor(dirs, device=cur_device).repeat(pos_token.shape[0], 1)
 
     return nl_value, nl_depth, nl_pos
+
+
+def preprocess(precondition, precondition_resolution, spatial_dim, device):
+    """ Transform input array elements into token sequences.
+
+    Args:
+        precondition: An array of elements (pixels/voxels) as an numpy array.
+        precondition_resolution: Resolution, to which the input array will be downscaled and used as a precondition.
+        spatial_dim: The spatial dimensionality of the array of elements.
+        device: Device on which, the data should be stored. Either "cpu" or "cuda" (gpu-support).
+
+    Return:
+        PyTorch tensor consisting of token sequences: (value, depth, position).
+    """
+    # convert input array into token sequence
+    tree = kdTree(spatial_dim)
+    tree = tree.insert_element_array(precondition, max_depth=math.log2(precondition_resolution) + 1)
+    value, depth, pos = tree.get_token_sequence(
+        depth=math.log2(precondition_resolution), return_depth=True, return_pos=True
+    )
+
+    # TODO: define trinary transformation based on list of embeddings
+
+    # convert sequence tokens to PyTorch as a long tensor
+    value = torch.tensor(value, dtype=torch.long, device=device)
+    depth = torch.tensor(depth, dtype=torch.long, device=device)
+    pos = torch.tensor(pos, dtype=torch.long, device=device)
+
+    return value, depth, pos
+
+
+def postprocess(value, target_resolution, spatial_dim):
+    """ Transform sequence of value tokens into an array of elements (voxels/pixels).
+
+    Args:
+        value: Value token sequence as a pytorch tensor.
+        target_resolution: Resolution up to which an object should be sampled.
+        spatial_dim: The spatial dimensionality of the array of elements.
+
+    Return:
+        An array of elements as a numpy array.
+    """
+    # move value sequence to the cpu and convert to numpy array
+    value = value.cpu().numpy()
+
+    # TODO: define trinary transformation based on list of embeddings
+
+    # insert the sequence into a kd-tree
+    tree = kdTree(spatial_dim).insert_token_sequence(
+        value,
+        resolution=target_resolution,
+        autorepair_errors=True,
+        silent=True,
+    )
+
+    # retrive pixels/voxels from the kd-tree
+    return tree.get_element_array(mode="occupancy")
