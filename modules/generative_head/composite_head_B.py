0612e9b91861ba01b214b132c28b3fb660def901
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2021-08-09 13:01:47 +0200

    update 'composite_B' to inherit from 'A'
    - inherit forward function from 'A'

--- modules/generative_head/composite_head_B.py
+++ modules/generative_head/composite_head_B.py
@@ -1,14 +1,13 @@
-import torch
 import torch.nn as nn
-from torch.nn.utils.rnn import pad_sequence
 
+from .composite_head_A import CompositeHeadA
 from .linear_head import LinearHead
 from .convolution_head_A import ConvolutionHeadA
 from .substitution_head import SubstitutionHead
 from .double_substitution_head import DoubleSubstitutionHead
 
 
-class CompositeHeadB(nn.Module):
+class CompositeHeadB(CompositeHeadA):
     def __init__(self, num_vocab, embed_dim, resolution, spatial_dim, **_):
         """ Performs a transformation from transformer latent space into target value logits.
 
@@ -21,7 +20,7 @@ class CompositeHeadB(nn.Module):
             resolution: Spatial resolution of sequence encoding.
             spatial_dim: Spatial dimension (2D/3D) of the sequence data.
         """
-        super(CompositeHeadB, self).__init__()
+        super(CompositeHeadB, self).__init__(num_vocab, embed_dim, resolution, spatial_dim)
 
         kwargs = {
             "num_vocab": num_vocab,
@@ -57,89 +56,3 @@ class CompositeHeadB(nn.Module):
             6: 2**spatial_dim,  # Note: 'substitution'
             7: 2**spatial_dim,  # Note: 'double_substitution'
         }
-
-    def forward(self, x, value, depth, position):
-        """ Transforms the output of the transformer target value logits.
-
-        Args:
-            x: Output of the transformer, the latent vector [N, T, E].
-            value: Target value token sequence [N, T].
-            depth: Target depth token sequence [N, T].
-            position: Target position token sequence [N, T, A].
-
-        Return
-            Logits of target value sequence.
-        """
-        batch_depth = torch.max(depth)
-        out = []
-
-        # process each sample individually
-        for latent_vec, val, dep, pos in zip(x, value, depth, position):
-            logits = torch.tensor([], device=x.device)
-            vector_idx = 0
-
-            # compute logits layerwise
-            for layer_idx, head in enumerate(self.heads):
-                layer_depth = layer_idx + 1
-                if layer_depth > batch_depth:
-                    break  # reached max depth layer
-
-                if layer_depth < 6:
-                    # get value, depth and position sequence of current layer
-                    layer_val = val[dep == layer_depth]
-                    layer_dep = dep[dep == layer_depth]
-                    layer_pos = pos[dep == layer_depth]
-                    # compute number of vectors in latent vector of current layer
-                    num_vectors = torch.sum(dep == layer_depth) // self.reduction_factor[layer_depth]
-                elif layer_depth == 6:  # handle substitution
-                    # get value, depth and position sequence of previous and current layer
-                    layer_val = torch.cat([val[dep == (layer_depth - 1)], val[dep == layer_depth]])
-                    layer_dep = torch.cat([dep[dep == (layer_depth - 1)], dep[dep == layer_depth]])
-                    layer_pos = torch.cat([pos[dep == (layer_depth - 1)], pos[dep == layer_depth]])
-                    # compute number of vectors in latent vector of current layer
-                    num_vectors = torch.sum(dep == (layer_depth - 1)) // self.reduction_factor[layer_depth]
-                elif layer_depth == 7:  # handle substitution
-                    # get value, depth and position sequence of previous and current layer
-                    layer_val = torch.cat(
-                        [
-                            val[dep == (layer_depth - 2)],
-                            val[dep == (layer_depth - 1)],
-                            val[dep == layer_depth],
-                        ]
-                    )
-                    layer_dep = torch.cat(
-                        [
-                            dep[dep == (layer_depth - 2)],
-                            dep[dep == (layer_depth - 1)],
-                            dep[dep == layer_depth],
-                        ]
-                    )
-                    layer_pos = torch.cat(
-                        [
-                            pos[dep == (layer_depth - 2)],
-                            pos[dep == (layer_depth - 1)],
-                            pos[dep == layer_depth],
-                        ]
-                    )
-                    # compute number of vectors in latent vector of current layer
-                    num_vectors = torch.sum(dep == (layer_depth - 2)) // self.reduction_factor[layer_depth]
-
-                # filter latent vector of current layer
-                layer_vec = latent_vec[vector_idx:vector_idx + num_vectors]
-
-                # compute layer logits
-                layer_logits = head(
-                    layer_vec.unsqueeze(0),
-                    layer_val.unsqueeze(0),
-                    layer_dep.unsqueeze(0),
-                    layer_pos.unsqueeze(0),
-                )[0]
-                logits = torch.cat([logits, layer_logits])
-
-                # discard processed tokens
-                vector_idx += num_vectors
-
-            out += [logits]
-
-        # pad embedding sequence
-        return pad_sequence(out, batch_first=True, padding_value=0.0)
